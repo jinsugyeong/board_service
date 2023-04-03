e52d947491f39ba6212238839299291f3075565d
jinsugyeong <apr15th@naver.com>
2023-04-03 21:15:45 +0900

    뷰 앤드포인트 테스트 정의

---
+++ board_service/src/main/java/com/fastcampus/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package com.fastcampus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+}
