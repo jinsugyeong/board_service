4ca0c88437b6ca4b7bf454e658b1f321d984c357
jinsugyeong <apr15th@naver.com>
2023-04-04 12:31:42 +0900

    계정 도메인 추가 작업

--- board_service/src/main/java/com/fastcampus/controller/ArticleController.java
+++ board_service/src/main/java/com/fastcampus/controller/ArticleController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.fastcampus.domain.Article;
-
 @EnableWebMvc
 @RequestMapping("/articles")
 @Controller
---
+++ board_service/src/main/java/com/fastcampus/repository/UserAccountRepository.java
@@ -0,0 +1,8 @@
+package com.fastcampus.repository;
+
+import com.fastcampus.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long>{
+
+}
