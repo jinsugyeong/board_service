17471864cab327a7cdde875b68a11af77a655484
jinsugyeong <apr15th@naver.com>
2023-04-10 17:06:22 +0900

    게시글 조회 컨트롤 구현

--- board_service/src/main/java/com/fastcampus/controller/ArticleController.java
+++ board_service/src/main/java/com/fastcampus/controller/ArticleController.java
@@ -2,29 +2,51 @@ package com.fastcampus.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@EnableWebMvc
+import com.fastcampus.domain.type.SearchType;
+import com.fastcampus.dto.response.ArticleCommentResponse;
+import com.fastcampus.dto.response.ArticleResponse;
+import com.fastcampus.dto.response.ArticleWithCommentResponse;
+import com.fastcampus.service.ArticleService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 	
+	private final ArticleService articleService;
+	
+	//게시글 리스트 조회
 	@GetMapping
-	public String articles(ModelMap map) {
-		map.addAttribute("articles", List.of());
+	public String articles(
+			@RequestParam(required = false) SearchType searchType,
+			@RequestParam(required = false) String searchValue,
+			@PageableDefault(size = 10, sort = "createAt", direction = Sort.Direction.DESC) Pageable pageable,
+			ModelMap map
+	) {
+		map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
 		
 		return "articles/index";
 	}
 	
+	//게시글 단건(상세) 조회
 	@GetMapping("/{articleId}")
 	public String article(@PathVariable Long articleId, ModelMap map) {
-		map.addAttribute("article", "");
-		map.addAttribute("articleComments", List.of());
+		ArticleWithCommentResponse article = ArticleWithCommentResponse.from(articleService.getArticle(articleId));
+		
+		map.addAttribute("article", article);
+		map.addAttribute("articleComments", article.articleCommentResponse());
 		
 		return "articles/detail";
 	}
--- board_service/src/test/java/com/fastcampus/controller/ArticleControllerTest.java
+++ board_service/src/test/java/com/fastcampus/controller/ArticleControllerTest.java
@@ -6,24 +6,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.BDDMockito.*;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fastcampus.config.SecurityConfig;
+import com.fastcampus.dto.ArticleWithCommentsDto;
+import com.fastcampus.dto.UserAccountDto;
+import com.fastcampus.service.ArticleService;
 
 @DisplayName("View 컨트롤러 - 게시글")
 @Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 	
-	@Autowired
 	private final MockMvc mvc;
+	
+	@MockBean private ArticleService articleService;
 	
 	public ArticleControllerTest(@Autowired MockMvc mvc) {
 		this.mvc = mvc;
@@ -33,6 +45,7 @@ class ArticleControllerTest {
 	@Test
 	public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
 		//Given
+		given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
 		
 		//When & Then
 		mvc.perform(get("/articles"))
@@ -40,20 +53,24 @@ class ArticleControllerTest {
 			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
 			.andExpect(view().name("articles/index"))
 			.andExpect(model().attributeExists("articles"));
+		then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
 	}
 	
 	@DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
 	@Test
 	public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
 		//Given
+		Long articleId = 1L;
+		given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 		
 		//When & Then
-		mvc.perform(get("/articles/1"))
+		mvc.perform(get("/articles/" + articleId))
 			.andExpect(status().isOk())
 			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
 			.andExpect(view().name("articles/detail"))
 			.andExpect(model().attributeExists("article"))
 			.andExpect(model().attributeExists("articleComments"));
+		then(articleService).should().getArticle(articleId);
 	}
 	
 	@Disabled("구현 중")
@@ -81,4 +98,33 @@ class ArticleControllerTest {
 			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
 			.andExpect(model().attributeExists("articles-hashtag"));
 	}
+	
+	 private ArticleWithCommentsDto createArticleWithCommentsDto() {
+	        return ArticleWithCommentsDto.of(
+	                1L,
+	                createUserAccountDto(),
+	                Set.of(),
+	                "title",
+	                "content",
+	                "#java",
+	                LocalDateTime.now(),
+	                "gang",
+	                LocalDateTime.now(),
+	                "gang"
+	        );
+	    }
+
+	    private UserAccountDto createUserAccountDto() {
+	        return UserAccountDto.of(1L,
+	                "sugyeong",
+	                "pw",
+	                "jsg@mail.com",
+	                "gang",
+	                "memo",
+	                LocalDateTime.now(),
+	                "gang",
+	                LocalDateTime.now(),
+	                "gang"
+	        );
+	    }
 }
