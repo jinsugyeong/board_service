67ded27e2f0d26d461a38c08af0b685a5b6835c6
jinsugyeong <apr15th@naver.com>
2023-04-18 16:51:32 +0900

    게시판 검색 Controller 테스트 작성 및 구현

--- board_service/src/test/java/com/fastcampus/controller/ArticleControllerTest.java
+++ board_service/src/test/java/com/fastcampus/controller/ArticleControllerTest.java
@@ -28,6 +28,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fastcampus.config.SecurityConfig;
+import com.fastcampus.domain.type.SearchType;
 import com.fastcampus.dto.ArticleWithCommentsDto;
 import com.fastcampus.dto.UserAccountDto;
 import com.fastcampus.service.ArticleService;
@@ -65,9 +66,33 @@ class ArticleControllerTest {
 		then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
 	}
 	
+	@DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 검색과 함께 호출")
+	@Test
+	public void givenSearchKeyword_whenSearctingArticlesView_thenReturnsArticlesView() throws Exception {
+		//Given
+		SearchType searchType = SearchType.TITLE;
+		String searchValue = "title";
+		given(articleService.searchArticles(eq(searchType), eq(searchValue), any(Pageable.class))).willReturn(Page.empty());
+		given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
+		
+		//When & Then
+		mvc.perform(
+				get("/articles")
+					.queryParam("searchType", searchType.name())
+					.queryParam("searchValue", searchValue)
+		)
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+			.andExpect(view().name("articles/index"))
+			.andExpect(model().attributeExists("articles"))
+			.andExpect(model().attributeExists("searchTypes"));
+		then(articleService).should().searchArticles(eq(searchType), eq(searchValue), any(Pageable.class));
+		then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+	}
+	
 	@DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 페이징, 정렬 기능")
 	@Test
-	void givenpagingAndSortingParams_whenSearchingArticlesPage_thenReturnsArticles() throws Exception {
+	void givenpagingAndSortingParams_whenSearchingArticlesView_thenReturnsArticlesView() throws Exception {
 		// Given
         String sortName = "title";
         String direction = "desc";
