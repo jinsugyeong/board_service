7c9786f3c9a24f7b3b87af7855cf53653002c7b2
jinsugyeong <apr15th@naver.com>
2023-04-29 17:30:36 +0900

    게시글 댓글 기능 controller 구현

--- board_service/src/main/java/com/fastcampus/controller/ArticleCommentController.java
+++ board_service/src/main/java/com/fastcampus/controller/ArticleCommentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fastcampus.dto.UserAccountDto;
 import com.fastcampus.dto.request.ArticleCommentRequest;
 import com.fastcampus.service.ArticleCommentService;
 
@@ -19,17 +20,23 @@ public class ArticleCommentController {
 	
 	
 	//댓글 생성
-	@PostMapping("/new")
-	public String postNewArticleComment(ArticleCommentRequest articleCommentReqest) {
-		
-		return "redirect:/articels";
-		
-	}
+	@PostMapping ("/new")
+    public String postNewArticleComment(ArticleCommentRequest articleCommentRequest) {
+        // TODO: 인증 정보를 넣어줘야 한다.
+        articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
+                "uno", "pw", "uno@mail.com", null, null
+        )));
+
+
+        return "redirect:/articles/" + articleCommentRequest.articleId();
+    }
 	
 	//댓글 삭제
-	@PostMapping("/{commentId}/deletre")
-	public String deleteArticleComment(@PathVariable Long commentId) {
-		
-		return "redirect:/articles";
-	}
+	@PostMapping ("/{commentId}/delete")
+    public String deleteArticleComment(@PathVariable Long commentId, Long articleId) {
+        articleCommentService.deleteArticleComment(commentId);
+
+        return "redirect:/articles/" + articleId;
+    }
+
 }
--- board_service/src/test/java/com/fastcampus/controller/ArticleCommentControllerTest.java
+++ board_service/src/test/java/com/fastcampus/controller/ArticleCommentControllerTest.java
@@ -45,45 +45,45 @@ class ArticleCommentControllerTest {
 
 
 	@DisplayName("[view][POST] 댓글 등록 - 정상 호출")
-	@Test
-	void givenArticleCommentInfo_whenRequesting_thenSavesNewArticleComment() throws Exception {
-		//Given
-		long articleId = 1L;
-		ArticleCommentRequest request = ArticleCommentRequest.of(articleId, "test comment");
-		willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
-		
-		//When & Then
-		mvc.perform(
-				post("/comments/new")
-					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-					.content(formDataEncoder.encode(request))
-					.with(csrf())
-		)
-			.andExpect(status().is3xxRedirection())
-			.andExpect(view().name("redirect:/articles/" + articleId))
-			.andExpect(redirectedUrl("/articles/" + articleId));
-		then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
-	}
-	
-	@DisplayName("[view][POST] 댓글 삭제 - 정상 호출")
-	@Test
-	void givenArticleCommentIdToDelete_whenRequesting_thenDeletesArticleComment() throws Exception {
-		//Given
-		long articleId = 1L;
-		long articleCommentId = 1L;
-		willDoNothing().given(articleCommentService).deleteArticleComment(articleCommentId);
-		
-		//When & Then
-		mvc.perform(
-				post("/comments/" + articleCommentId + "/delete")
-					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-					.content(formDataEncoder.encode(Map.of("articleId", articleId)))
-					.with(csrf())
-		)
-			.andExpect(status().is3xxRedirection())
-			.andExpect(view().name("redirect:/articles/" + articleId))
-			.andExpect(redirectedUrl("/articles/" + articleId));
-		then(articleCommentService).should().deleteArticleComment(articleCommentId);
-	}
+    @Test
+    void givenArticleCommentInfo_whenRequesting_thenSavesNewArticleComment() throws Exception {
+        // Given
+        long articleId = 1L;
+        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, "test comment");
+        willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
+
+        // When & Then
+        mvc.perform(
+                post("/comments/new")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(request))
+                        .with(csrf())
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
+    }
+
+    @DisplayName("[view][GET] 댓글 삭제 - 정상 호출")
+    @Test
+    void givenArticleCommentIdToDelete_whenRequesting_thenDeletesArticleComment() throws Exception {
+        // Given
+        long articleId = 1L;
+        long articleCommentId = 1L;
+        willDoNothing().given(articleCommentService).deleteArticleComment(articleCommentId);
+
+        // When & Then
+        mvc.perform(
+                post("/comments/" + articleCommentId + "/delete")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(Map.of("articleId", articleId)))
+                        .with(csrf())
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+        then(articleCommentService).should().deleteArticleComment(articleCommentId);
+    }
 
 }
