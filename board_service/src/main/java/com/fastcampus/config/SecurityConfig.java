5ff974aead2a35b20afaf0e4c2a2770bc6dee8e7
jinsugyeong <apr15th@naver.com>
2023-04-30 14:32:58 +0900

    인증 기능 구현 1

--- board_service/src/main/java/com/fastcampus/config/SecurityConfig.java
+++ board_service/src/main/java/com/fastcampus/config/SecurityConfig.java
@@ -2,8 +2,17 @@ package com.fastcampus.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.fastcampus.dto.UserAccountDto;
+import com.fastcampus.dto.security.BoardPrincipal;
+import com.fastcampus.repository.UserAccountRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -11,9 +20,31 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth
+                		.requestMatchers(
+                				HttpMethod.GET,
+                				"/"
+                				,"/articles"
+                				,"/articles/search-hashtag"
+                		).permitAll()
+                		.anyRequest().authenticated()
+                )
                 .formLogin().and()
                 .build();
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
