97755d062ca564a0d27df028cc4bb8b4aac6333f
jinsugyeong <apr15th@naver.com>
2023-05-03 14:22:55 +0900

    테스트 코드 수정

--- board_service/src/test/java/com/fastcampus/controller/AuthControllerTest.java
+++ board_service/src/test/java/com/fastcampus/controller/AuthControllerTest.java
@@ -1,14 +1,15 @@
 package com.fastcampus.controller;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.BDDMockito.then;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
@@ -20,7 +21,7 @@ import com.fastcampus.service.PaginationService;
 
 @DisplayName("View 컨트롤러 - 인증")
 @Import(TestSecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 class AuthControllerTest {
 	@Autowired
 	private final MockMvc mvc;
@@ -44,4 +45,10 @@ class AuthControllerTest {
 		then(articleService).shouldHaveNoInteractions();
 		then(paginationService).shouldHaveNoInteractions();
 	}
+	
+    /**
+     * 어떤 컨트롤러도 필요하지 않은 테스트임을 나타내기 위해 테스트용 빈 컴포넌트를 사용함.
+     */
+    @TestComponent
+    static class EmptyController {}
 }
