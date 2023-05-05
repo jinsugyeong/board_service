635b302ef6ad69f5a4b01e0244f6f21fe5c06eab
jinsugyeong <apr15th@naver.com>
2023-05-06 00:19:02 +0900

    도메인 변경점의 반영: 해시태그 : queryDSL 빌드 오류

--- board_service/src/main/java/com/fastcampus/dto/request/ArticleRequest.java
+++ board_service/src/main/java/com/fastcampus/dto/request/ArticleRequest.java
@@ -1,23 +1,29 @@
 package com.fastcampus.dto.request;
 
+import java.util.Set;
+
 import com.fastcampus.dto.ArticleDto;
+import com.fastcampus.dto.HashtagDto;
 import com.fastcampus.dto.UserAccountDto;
 
 public record ArticleRequest(
 		 String title
 		,String content
-		,String hashtag
 ) {
-	public static ArticleRequest of(String title, String content, String hashtag) {
-		return new ArticleRequest(title, content, hashtag);
+	public static ArticleRequest of(String title, String content) {
+		return new ArticleRequest(title, content);
 	}
 	
 	public ArticleDto toDto(UserAccountDto userAccountDto) {
+		return toDto(userAccountDto, null);
+	}
+	
+	public ArticleDto toDto(UserAccountDto userAccountDto, Set<HashtagDto> hashtagDtos) {
 		return ArticleDto.of(
 				userAccountDto
 				, title
 				, content
-				, hashtag
+				, hashtagDtos
 		);
 	}
 }
--- board_service/src/main/java/com/fastcampus/repository/querydsl/ArticleRepositoryCustom.java
+++ board_service/src/main/java/com/fastcampus/repository/querydsl/ArticleRepositoryCustom.java
@@ -2,6 +2,8 @@ package com.fastcampus.repository.querydsl;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 public interface ArticleRepositoryCustom {
-	List<String> findAllDistinctHashtags();
+	List<String> findAllDistinctHashtags(String string, Pageable pageable);
 }
